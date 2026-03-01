586a478f2199c80202c6abe13f4e4cc01f978822
Andres <alcivar.andres001@hotmail.com>
2026-03-01 15:47:31 -0500

    se pone comentario al jwt guard

--- src/user/jwt/jwt-auth.guard.ts
+++ src/user/jwt/jwt-auth.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+//habilitado para jwt
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
